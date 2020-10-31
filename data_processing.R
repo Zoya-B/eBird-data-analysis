1ec648179f37edd0f8116fe10ccc7dd6bc282dc9
Zoya Buckmire <buckmirz@myumanitoba.ca>
2020-10-31 14:07:40 -0400

    Complete processing and filtering of data

--- data_processing.R
+++ data_processing.R
@@ -3,9 +3,56 @@ library(tidyverse)
 library(dplyr)
 library(ggplot2)
 
-obs <- read.csv("Obs_by_date.csv", header = TRUE, sep = ",")
+obs <- read.csv("Obs_complete.csv", header = TRUE, sep = ",")
+obs <- obs[1:5852,] #file imported with empty rows from original unfiltered dataset
 obs <- janitor::clean_names(obs)
 
+obs$observation_date<-as.Date(obs$observation_date, format = "%d/%m/%Y")
 obs$observation_count <- as.integer(obs$observation_count)
 obs$site <- as.factor(obs$site)
 obs$timeline <- as.factor(obs$timeline)
+
+total_richness <- n_distinct(obs$common_name)
+
+summary <- obs %>% group_by(site) %>%
+  summarize(richness = n_distinct(common_name), checklists = n_distinct(time_observations_started), 
+            avg_observers = round(mean(number_observers, na.rm = TRUE)), 
+            max_observers = max(number_observers, na.rm = TRUE),
+            avg_distance = mean(effort_distance_km, na.rm = TRUE),
+            max_distance = max(effort_distance_km, na.rm = TRUE), 
+            avg_duration = mean(duration_minutes, na.rm = TRUE), 
+            min_length = min(duration_minutes, na.rm = TRUE), 
+            max_length = max(duration_minutes, na.rm = TRUE))
+
+#to account for variability in detectability (https://cornelllabofornithology.github.io/ebird-best-practices/ebird.html#ebird-detect)
+#filter for checklist with < 10 observers, < 5 hours long and <5 km travelled
+
+obs_filtered <- obs %>% 
+  filter(duration_minutes <= 5*60, effort_distance_km <= 5, number_observers <= 10)
+
+summary_postfilter <- obs_filtered %>% group_by(site) %>%
+  summarize(richness = n_distinct(common_name), checklists = n_distinct(time_observations_started), 
+            avg_observers = round(mean(number_observers, na.rm = TRUE)), 
+            max_observers = max(number_observers, na.rm = TRUE),
+            avg_distance = mean(effort_distance_km, na.rm = TRUE),
+            max_distance = max(effort_distance_km, na.rm = TRUE), 
+            avg_duration = mean(duration_minutes, na.rm = TRUE), 
+            min_length = min(duration_minutes, na.rm = TRUE), 
+            max_length = max(duration_minutes, na.rm = TRUE))
+
+
+lev_obs <- filter(obs_filtered, site %in% "LEV")
+lev_obs %>% group_by(timeline) %>%
+  summarize(checklists = n_distinct(time_observations_started), 
+            richness = n_distinct(common_name), proportion = richness/70)
+
+lsg_obs <- filter(obs_filtered, site %in% "LSG")
+lsg_obs %>% group_by(timeline) %>%
+  summarize(checklists = n_distinct(time_observations_started), 
+            richness = n_distinct(common_name), proportion = richness/79)
+
+mth_obs <- filter(obs_filtered, site %in% "MTH")
+mth_obs %>% group_by(timeline) %>%
+  summarize(checklists = n_distinct(time_observations_started), 
+            richness = n_distinct(common_name), proportion = richness/59)
+            
